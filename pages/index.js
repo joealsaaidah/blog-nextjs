118819af56e89fe9463023130f41187b0aa948b2
joealsaaidah <alsaaidahyousef@gmail.com>
2022-06-28 18:45:52 +0800

    create next project and add tailwind

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <h1 className='text-6xl text-blue-300 font-bold underline'>Hello world!</h1>
+  );
+}
